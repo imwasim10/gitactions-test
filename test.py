ae9794d5d8af2bb65adb792534bd2dfa2426b2b3
Wasim <73107352+imwasim10@users.noreply.github.com>
2023-02-22 20:41:10 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+# errors in code:
+# line 17 'myage' var not defined
+
+
+# CODE START #
+
+print("Hello World!")
+
+print("What is your name?")
+
+myName = input()
+
+print("It is good to meet you, " + myName)
+
+print("What is your age?")
+
+myAge = input()
+
+print("Your age is " + myAge)
